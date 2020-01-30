87b337b5693eb751177252d8764395a0ffbb65ab
sabreensalama <sabreensalama29@gmail.com>
2020-01-30 06:38:54 +0200

    delete record

---
+++ delete_record.sh
@@ -0,0 +1,56 @@
+
+function get_Data
+{
+  mydata=$(sed '1,2d' "$filename")
+  echo "$mydata"
+  echo "enter the line you want to delete"
+  read line
+    if [[ $line -eq 0 ]]
+    then
+    echo "you have no record"
+    fi 
+  if [[ "$line" =~ ^[1-9]+$ ]]
+  then
+  line=$((line+2))
+   #i to remove from source
+    sed -i "$line d" "$filename"
+    echo "your record is deleted "
+  elif [[ -z "$line" ]]
+  then
+  echo "must not be empty"
+  elif [[ "$line" =~ ^[a-zA-Z]+$ ]]
+  then
+  echo "must be numeric "
+
+ else
+  echo "not valid"
+ fi
+  
+}
+
+function file_exist
+{
+   if test -f "$filename"
+   then
+   return 1
+   else
+   return 0
+   fi
+
+}
+
+
+
+echo "enter your file:"
+read filename
+file_exist
+
+#to check status
+result="$?"
+if [[ "$result" -eq 1 ]]
+then
+echo "your file found"
+  get_Data
+else
+echo "your file does not exist"
+fi
