38a1a06a6e26adb2c3b760feb4dc945da7fec1d1
mo7amedsalah <mohamed.s.abokhadra@gmail.com>
2020-01-31 21:28:44 +0200

    View table content

---
+++ view_table.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+LC_ALL=C
+shopt -s extglob
+clear
+echo "Enter Table Name!!!!!!!!!!!";
+IFS= read -r table_name
+case $table_name in 
+        #validate that the name must have not white spaces..
+              
+	*\ *)
+            echo "-----------------------------------------------------------------------"; 
+            echo "Invalid Table name!! the name must be without no spaces..!! "
+	  ;;
+	#validate that the name must be letters only..
+	+([[:digit:]])) 
+            echo "-----------------------------------------------------------------------"; 
+            echo "Invalid Table name!! the name must be letters and do not have whitespaces!!!!!" 
+          ;;
+	#the valid regix for name..	
+	+([a-zA-Z])) 
+	
+		#IF table is Exist show the structure...
+	   if source check_table_exist.sh; then 
+            echo "-----------------------------------------------------------------------";
+	    echo "This are The names of columns in $table_name";
+            head -1 $table_name;
+	    echo "-----------------------------------------------------------------------";
+	    echo "This is The data type for every column in order of table $table_name";
+	    echo "-----------------------------------------------------------------------";
+	     awk 'NR == 2 { print }' $table_name;
+	    echo "-----------------------------------------------------------------------";
+	    echo "This is The data for every column in order of table $table_name";
+              awk 'NR > 2 { print }' $table_name;
+	   else 
+		#IF table is not exist
+	      
+	      echo "-----------------------------------------------------------------------";	
+	      echo "The table is not exist";
+	   fi
+	  ;;
+
+	*) echo "Invalid Table name!! name of Table must be lower or upper letters or mix and do not have whitespaces!!!! "
+esac
+
+
